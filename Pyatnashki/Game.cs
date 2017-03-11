adab303b983e18f84d396f24ae9f8445edca5794
Ильян <ilyan03011@gmail.com>
2017-03-11 20:11:40 +0500

    Пятнашки(работа над рэндом)

---
+++ Pyatnashki/Game.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pyatnashki
+{
+    class Game
+    {
+        int[,] fifteen;
+        int zeroX;
+        int zeroY;
+        int nX = 0;
+        int nY = 0;
+
+
+        public Game(int length)
+        {
+            fifteen = new int[length, length];
+            FillArray();
+            PrintConsole.ShowTable(fifteen);
+            Moving();
+        }
+        public void Randoming (int[,] fifteen)
+        {
+            Random Gen = new Random();
+            int iran = 0;
+            int jran = 0;
+
+            for (int i = 0; i < fifteen.GetLength(0); i++)
+            {
+                for (int j = 0; j < fifteen.GetLength(1); j++)
+                {
+                    iran = Gen.Next(0, fifteen.GetLength(0));
+                    jran = Gen.Next(0, fifteen.GetLength(1));
+
+                    int temp = fifteen[iran, jran];
+                    fifteen[iran, jran] = fifteen[i, j];
+                    fifteen[i, j] = temp;
+                }
+            }
+        }
+
+        public void FillArray()
+        {
+            int count = 0;
+            for (int i = 0; i < fifteen.GetLength(0); i++)
+            {
+                for (int j = 0; j < fifteen.GetLength(1); j++)
+                {
+                    fifteen[i, j] = count;
+                    count++;
+
+                    if (fifteen[i, j] == 0)
+                    {
+                        zeroX = i;
+                        zeroY = j;
+                    }
+                }
+            }
+        }
+
+        public void Moving()
+        {
+            int n = 0;
+
+            do
+            {
+                PrintConsole.Warning();
+                n = Convert.ToInt32(Console.ReadLine());
+
+            } while (CheckZero(n) != true);
+
+            int temp = fifteen[zeroX, zeroY];
+            fifteen[zeroX, zeroY] = fifteen[nX, nY];
+            fifteen[nX, nY] = temp;
+            
+            PrintConsole.ShowTable(fifteen);
+
+            Moving();
+            
+        }
+
+        public bool CheckZero(int n)
+        {
+            for (int i = 0; i < fifteen.GetLength(0); i++)
+            {
+                for (int j = 0; j < fifteen.GetLength(1); j++)
+                {
+                    if (fifteen[i,j] == n)
+                    {
+                        nX = i;
+                        nY = j;
+                    }
+                }
+            }
+
+            if (Math.Sqrt(Math.Pow(nX-zeroX,2)+ Math.Pow(nY - zeroY, 2)) == 1)
+            {
+                int temp = zeroX;
+                int temp2 = zeroY;
+                zeroX = nX;
+                zeroY = nY;
+                nX = temp;
+                nY = temp2;
+                
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ Pyatnashki/PrintConsole.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pyatnashki
+{
+    class PrintConsole
+    {
+        public static void ShowTable(int[,] fifteen)
+        {
+            Console.Clear();
+            for (int i = 0; i < fifteen.GetLength(0); i++)
+            {
+                for (int j = 0; j < fifteen.GetLength(1); j++)
+                {
+                    Console.Write(fifteen[i,j] + " "); 
+                }
+                Console.WriteLine();
+            }
+
+        }
+
+        public static void Warning()
+        {
+            Console.Write("С каким числом поменять нолик ");
+        }
+
+
+    }
+}
